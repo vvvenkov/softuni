31a5ca4c0642476d660dcdff30673d9275c773b3
vvvenkov <venelin.venkov@protonmail.com>
2024-01-14 20:13:22 +0200

    .

---
+++ 03jsAdvanced/01syntaxFunctionsAndStatements/01syntaxFunctionsAndStatementsLab/06sumOfNumbersNM.js
@@ -0,0 +1,9 @@
+function sumofNumbersMN(num1, num2) {
+    let sum = 0;
+
+    for (let i = Number(num1); i <= Number(num2); i++) {
+        sum += i;
+    }
+    console.log(sum);
+}
+sumofNumbersMN("-8", "20")
