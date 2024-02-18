f36bac66e2784a8e1e7d613d173170e3207f06c5
vvvenkov <venelin.venkov@protonmail.com>
2024-02-18 13:07:50 +0200

    .

--- 03jsAdvanced/04domIntroduction/01domIntroductionLab/01.Edit Element/EditElement.js
+++ 03jsAdvanced/04domIntroduction/01domIntroductionLab/01.Edit Element/EditElement.js
@@ -1,3 +1,7 @@
-function editElement() {
-    // TODO
+function editElement(ref, match, replacer)  {
+    let text = ref.textContent;
+
+    text = text.replace(match, replacer);
+
+    ref.textContent = text;
 }
--- 03jsAdvanced/04domIntroduction/01domIntroductionLab/02.Collect List Items/CollectListItems.js
+++ 03jsAdvanced/04domIntroduction/01domIntroductionLab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,11 @@
 function extractText() {
-    // TODO
+    const list = document.getElementById("items");
+    const itemArray = Array.from(list.children);
+
+    const result = itemArray
+        .map(li => li.textContent)
+        .join("\n");
+
+    const output = document.getElementById("result");
+    output.value = result;
 }
