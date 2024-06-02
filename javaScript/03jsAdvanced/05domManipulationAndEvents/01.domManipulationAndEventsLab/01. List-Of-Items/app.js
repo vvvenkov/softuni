d3ec34d567ae36d5591417deddfc6af98d750045
vvvenkov <venelin.venkov@protonmail.com>
2024-06-02 19:00:15 +0300

    update

--- javaScript/03jsAdvanced/05domManipulationAndEvents/01.domManipulationAndEventsLab/01. List-Of-Items/app.js
+++ javaScript/03jsAdvanced/05domManipulationAndEvents/01.domManipulationAndEventsLab/01. List-Of-Items/app.js
@@ -1,3 +1,19 @@
 function addItem() {
-    console.log('TODO:...');
+    //get related elements from DOM
+    const input = document.getElementById("newItemText");
+    const ul = document.getElementById("items");
+
+    //create new li element 
+    const li = document.createElement("li");
+    li.textContent = input.value;
+
+    //append li element to DOM
+    ul.append(li);
+
+    //clear the entry bar
+    input.value = "";
+
+    //focus on input 
+    input.focus();
+
 }
